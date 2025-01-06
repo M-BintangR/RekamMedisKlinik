80467766110c932e1bf300650805df6d75de6232
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-06 11:37:30 +0800

    [dev] login component

--- RekamMedisKlinik/Form1.Designer.cs
+++ RekamMedisKlinik/Form1.Designer.cs
@@ -28,18 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(37, 170);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(334, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 145);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 1;
+            label1.Text = "EMAIL";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(40, 268);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(331, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 238);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 3;
+            label2.Text = "PASSWORD";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(40, 338);
+            button1.Name = "button1";
+            button1.Size = new Size(331, 36);
+            button1.TabIndex = 4;
+            button1.Text = "Masuk";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(40, 392);
+            button2.Name = "button2";
+            button2.Size = new Size(331, 36);
+            button2.TabIndex = 5;
+            button2.Text = "Reset";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(405, 495);
+            ClientSize = new Size(422, 495);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private Button button2;
     }
 }
